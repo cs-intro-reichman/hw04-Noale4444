b9d1c4a06126374a949c228b28d5608eeebb633b
Noale4444 <noa.bendor@post.runi.ac.il>
2024-01-13 14:21:36 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -6,18 +6,18 @@ public class StringOps {
     //////                                               ///////
     //////        1.charAt(int index)                    ///////
     //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
+    //////        3.subString(int start)                 ///////
+    //////        4.subString(int start,int ends)        ///////
+    //////        5.indexOf(String s)                  ///////
     //////                                               ///////
     //////        The rest are not allowed !             ///////
     //////        if you want to use a different         ///////
     //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
+    //////        it uString material from the course      ///////
     //////        you need to implement a version of     ///////
     //////        the function by yourself.              ///////
     //////                                               ///////
-    //////        see example for substring              ///////
+    //////        see example for subString              ///////
     //////        in Recitation 3 question 5             ///////
     //////                                               ///////
     ////////////////////////////////////////////////////////////
@@ -25,21 +25,21 @@ public class StringOps {
      
     }
 
-    public static String capVowelsLowRest(String string) {
+    public static String capVowelsLowRest(String String) {
       String vowels = "aeiou"; 
       String VOWELS = "AEIOU";
       String s1 = "";
-      for(int i = 0; i < string.length(); i++){
-        if(vowels.indexOf(string.charAt(i)) != -1)
-           s1 += (char) (string.charAt(i) - 32); 
-            else if(VOWELS.indexOf(string.charAt(i)) != -1) {
-                s1+= string.charAt(i);
+      for(int i = 0; i < String.length(); i++){
+        if(vowels.indexOf(String.charAt(i)) != -1)
+           s1 += (char) (String.charAt(i) - 32); 
+            else if(VOWELS.indexOf(String.charAt(i)) != -1) {
+                s1+= String.charAt(i);
               }  else {
-                if(vowels.indexOf(string.charAt(i)) == -1 && VOWELS.indexOf(string.charAt(i)) == -1) {
-                  }  if(string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') {
-                     s1 += (char) (string.charAt(i) + 32);
+                if(vowels.indexOf(String.charAt(i)) == -1 && VOWELS.indexOf(String.charAt(i)) == -1) {
+                  }  if(String.charAt(i) >= 'A' && String.charAt(i) <= 'Z') {
+                     s1 += (char) (String.charAt(i) + 32);
                        }  else  {
-                         s1+= string.charAt(i);  
+                         s1+= String.charAt(i);  
                    }
                   }
                 } 
@@ -48,77 +48,68 @@ public class StringOps {
     }
 
 
-     public static String firstToLower(String string) {
-      String s = ""; 
-      int count = 0; 
-      int start = 0; 
-      while (string.charAt(start) == ' ') {
-        count++;
-        start++;
+    public static String firstToLower(String String) {
+      String s = "";
+      int start = 0;
+      int count = 0;
+      while (String.charAt(start) == 32) {
+          count++;
+          start++;
       }
-       for(int i = count; i < string.length();i++){
-        if(string.charAt(i) >= 'A' && string.charAt(i) <= 'Z')
-         s+= (char) (string.charAt(i) + 32);
-         else
-         s += string.charAt(i);
-        }
-          return s;
+      for (int i = count; i < String.length(); i++) {
+          if (String.charAt(i) >= 'A' && String.charAt(i) <= 'Z')
+              s += (char) (String.charAt(i) + 32);
+          else
+              s += string.charAt(i);
       }
+      return s;
+  }
 
-      
-
-      public static String firstToUpper(String string) { 
-        String s = "";
-        s += string.charAt(0); 
-        for(int i = 1; i < string.length();i++) { 
-          if(string.charAt(i - 1) == ' ') {
-            if(string.charAt(i) >= 'a' && string.charAt(i) <= 'z')
-             s+= (char) (string.charAt(i) - 32);
-           else 
-             s+= string.charAt(i); 
-          }
-        else if(string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') 
-          s+= (char) (string.charAt(i) + 32);
-          else 
-           s+= string.charAt(i);
-
+  public static String firstToUpper(String String) {
+      String s = "";
+      s += String.charAt(0);
+      for (int i = 1; i < String.length(); i++) {
+          if (String.charAt(i - 1) == ' ') {
+              if (String.charAt(i) >= 'a' && String.charAt(i) <= 'z')
+                  s += (char) (String.charAt(i) - 32);
+              else
+                  s += String.charAt(i);
+          } else if (String.charAt(i) >= 'A' && String.charAt(i) <= 'Z')
+              s += (char) (String.charAt(i) + 32);
+          else
+              s += String.charAt(i);
       }
-             return s; 
-        }
-         
-        
+      return s;
+  }
 
-       public static String deletSpaces(String string) { 
-        String s = ""; 
-        for(int i = 0; i < string.length(); i++) { 
-          if(string.charAt(i) != ' ' ) 
-            s+= string.charAt(i);
-        }
-        return s; 
-       }
+  public static String deleteSPaces(String String) {
+      String s = "";
+      for (int i = 0; i < String.length(); i++) {
+          if (String.charAt(i) != 32)
+              s += String.charAt(i);
+      }
+      return s;
+  }
 
-
-       public static String camelCase(String string) {
-        string = firstToLower(string);
-        string = firstToUpper(string);
-        string = deletSpaces(string);
-     
-        return string;
-     }
-     
+  public static String camelCase(String String) {
+      String = firstToLower(String);
+      String = firstToUpper(String);
+      String = deleteSPaces(String);
+      return String;
+  }
 
 
   
-    public static int[] allIndexOf (String string, char chr) {
+    public static int[] allIndexOf (String String, char chr) {
      int arrayLength = 0;
-     for(int i = 0; i < string.length(); i++) { 
-      if(string.charAt(i) == chr) 
+     for(int i = 0; i < String.length(); i++) { 
+      if(String.charAt(i) == chr) 
       arrayLength++;
      }     
      int curIndex = 0; 
      int[] array = new int[arrayLength] ; 
-     for(int i = 0; i < string.length(); i++){
-      if(string.charAt(i) == chr) {
+     for(int i = 0; i < String.length(); i++){
+      if(String.charAt(i) == chr) {
       array[curIndex] = i; 
       curIndex++;
      } 
